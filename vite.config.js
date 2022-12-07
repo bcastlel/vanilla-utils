452b4c16cf7e08e9c4b89a91d5eeb3668f0ed6a2
bcastlel <bcastlel@users.noreply.github.com>
2022-12-07 04:33:39 +0400

    init project

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
+
+export default defineConfig({
+  plugins: [
+    /* see https://github.com/gxmari007/vite-plugin-eslint#options */
+    eslint({ fix: true, include: ['src/**/*.js', 'src/**/*.ts'] }),
+  ],
+});
