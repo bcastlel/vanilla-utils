187f18fed8f3a4ee0fcb21594e992d8a8d691dfb
bcastlel <bcastlel@users.noreply.github.com>
2023-01-17 16:52:01 +0400

    add "get focusable element"

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import eslint from 'vite-plugin-eslint';
 export default defineConfig({
   plugins: [
     /* see https://github.com/gxmari007/vite-plugin-eslint#options */
-    eslint({ fix: true, include: ['src/**/*.js', 'src/**/*.ts'] }),
+    eslint({ fix: true }),
   ],
 });
